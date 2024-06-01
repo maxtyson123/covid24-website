12ffc7deb86eef88bae9f099cc086fe7f3ee2815
Max Tyson <98maxt98@gmail.com>
2024-06-01 17:49:12 +1200

    Inital Commit

---
+++ src/lib/db.ts
@@ -0,0 +1,84 @@
+import type {NextApiRequest, NextApiResponse} from "next";
+import axios from "axios";
+const sqlDb = require('mysql2-async').default;
+
+export async function GetUserID(req: NextApiRequest, res: NextApiResponse): Promise<any> {
+    const token = "Bearer ZDFINTI2NGETMJNKNI0ZZTC3LTK4NTETZMQXOTC5OWNLYWY3"; //req.headers.authorization;
+    if (!token) {
+        res.status(401).json({message: "Unauthorized"});
+        return;
+    }
+
+    // Get the user from the token
+    let userid = "";
+    try {
+        const config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: process.env.NEXT_PUBLIC_API_URL + "/oauth/userinfo",
+            headers: {
+                'Authorization': token
+            }
+        };
+
+        const user = await axios.request(config);
+        return user.data.user_id;
+    } catch (e) {
+        console.log(e);
+        res.status(401).json({message: "Unauthorized"});
+    }
+}
+
+export async function getClient(){
+
+    const dataBase = new sqlDb({
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT) : 1234,
+        database: process.env.MYSQL_DATABASE,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        skiptzfix: true,
+        multipleStatements: true,
+    });
+
+    let client: any = dataBase
+
+    return client
+}
+
+export function createUpSertQuery(table: string, columns: string[], data: any[])
+{
+    // Create the upsert query
+    let upsert_query = `INSERT INTO ${table} (`
+
+    // Add the columns
+    upsert_query += columns.join(", ")
+
+    // Add the values
+    upsert_query += `) VALUES (`
+
+    // Loop through the data
+    for(let i = 0; i < data.length; i++){
+        upsert_query += `'${data[i]}'`
+        if(i < data.length - 1){
+            upsert_query += ", "
+        }
+    }
+
+    // Add the ON DUPLICATE KEY UPDATE
+    upsert_query += `) ON DUPLICATE KEY UPDATE `
+
+    // Loop through the columns
+    for(let i = 0; i < columns.length; i++){
+        upsert_query += `${columns[i]} = VALUES(${columns[i]})`
+        if(i < columns.length - 1){
+            upsert_query += ", "
+        }
+    }
+
+    // Add the end of the query
+    upsert_query += ";"
+
+    return upsert_query
+
+}
---
+++ src/pages/api/user.ts
@@ -0,0 +1,73 @@
+import type {NextApiRequest, NextApiResponse} from "next";
+import axios from "axios";
+import {createUpSertQuery, getClient, GetUserID} from "@/lib/db";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse,
+) {
+
+
+    // Get the user ID
+    const userid = await GetUserID(req, res);
+
+    // Connect to the DB
+    const client = await getClient();
+    let query = ""
+
+    console.log("GOT CLIENT");
+
+    // Get the operation from the request
+    const operation = req.method;
+    switch (operation) {
+        case "GET":
+
+            // Fetch the data
+            query = `SELECT * FROM users WHERE uid = '${userid}'`;
+            break;
+
+        case "POST":
+
+            // Get the data
+            const {
+                level,
+                exp,
+                rexp,
+                totalPlays,
+                totalKills,
+                spentCash,
+                kills,
+            } = req.body;
+
+            // Update the data
+            query = createUpSertQuery("users",
+                ["uid", "updatedAt", "level", "exp", "rexp", "totalPlays", "totalKills", "spentCash", "kills"],
+                [userid, new Date().toISOString().slice(0, 19).replace('T', ' '), level, exp, rexp, totalPlays, totalKills, spentCash, kills]
+                )
+
+            break;
+
+    }
+
+    // Make the operation
+    console.log("MAKING QUERY");
+    console.log(query);
+    const data  = await client.getall(query);
+    console.log("QUERY DONE");
+
+    // Handle the response
+    switch (operation) {
+        case "GET":
+            // Check if the data exists
+            if (data.length === 0) {
+                res.status(404).json({message: "Not found"});
+                return;
+            }
+            res.status(200).json(data);
+            break;
+
+        case "POST":
+            res.status(201).json(data);
+            break;
+    }
+}
